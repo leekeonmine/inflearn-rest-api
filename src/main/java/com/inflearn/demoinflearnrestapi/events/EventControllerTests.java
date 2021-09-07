80480117f14de7c4e2ad45adb8a3c93531abb74c
mac <dudrjsxo3@gmail.com>
2021-09-07 16:20:03 +0900

    Ristrict input

--- src/main/java/com/inflearn/demoinflearnrestapi/events/EventControllerTests.java
+++ src/main/java/com/inflearn/demoinflearnrestapi/events/EventControllerTests.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
@@ -20,7 +22,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTests {
 
     @Autowired
@@ -29,23 +32,25 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
 
         Event event = Event.builder()
-                        .name("Spring")
-                        .description("REST API Development with Spring")
-                        .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
-                        .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,21))
-                        .beginEventDateTime(LocalDateTime.of(2018,11,25,14,21))
-                        .endEventDateTime(LocalDateTime.of(2018,11,26,14,21))
-                        .basePrice(100)
-                        .maxPrice(200).limitOfEnrollment(100).location("강남역").build();
-        event.setId(10);
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
+                .id(100)
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,21))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,14,21))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,21))
+                .basePrice(100)
+                .maxPrice(200).limitOfEnrollment(100).location("강남역")
+                .free(true)
+                .offline(false)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
+
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .accept(MediaTypes.HAL_JSON)
@@ -53,6 +58,7 @@ public class EventControllerTests {
                 )
                 .andDo(print())
             .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists());
+            .andExpect(jsonPath("id").exists())
+            .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
     }
 }
