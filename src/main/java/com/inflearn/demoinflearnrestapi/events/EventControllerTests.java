49259c13daec22b3ede6b1a1ca41e9cbd767d883
mac <dudrjsxo3@gmail.com>
2021-09-09 12:30:42 +0900

    hateos link add

--- src/main/java/com/inflearn/demoinflearnrestapi/events/EventControllerTests.java
+++ src/main/java/com/inflearn/demoinflearnrestapi/events/EventControllerTests.java
@@ -52,18 +52,21 @@ public class EventControllerTests {
                 .build();
 
         mockMvc.perform(post("/api/events/")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON)
-                .content(objectMapper.writeValueAsString(event))
-                )
-                .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("free").value(false))
-                .andExpect(jsonPath("offline").value(true))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event))
+                        )
+                        .andDo(print())
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("id").exists())
+                        .andExpect(header().exists(HttpHeaders.LOCATION))
+                        .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_UTF8_VALUE))
+                        .andExpect(jsonPath("free").value(false))
+                        .andExpect(jsonPath("offline").value(true))
+                        .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+                        .andExpect(jsonPath("_links.self").exists())
+                        .andExpect(jsonPath("_links.query-events").exists())
+                        .andExpect(jsonPath("_links.update-events").exists());
     }
 
 
